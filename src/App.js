57d1c675433756e6aaf87ce84304139da1ada5d8
takiguchi-yu <8340629+takiguchi-yu@users.noreply.github.com>
2019-12-01 02:40:48 +0900

    learn props of components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,20 @@ import React, { Component } from 'react';
 // 関数コンポーネント
 
 function App() {
+  const profiles = [
+    {
+      name: "Ichiro", 
+      age: 30,
+    },
+    {
+      name: "yamada",
+      age: 50,
+    },
+    {
+      name: "tanaka",
+    }
+  ]
+
   return (
     <React.Fragment>
       <h1 className="foo">Hello, world!!</h1>
@@ -11,12 +25,32 @@ function App() {
       <Cat />
       <Cat />
       <Cat />
+      <User name={"Taro"} age={10} />
+      <User name={"Hanalo"} age={5} />
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
     </React.Fragment>
-  );
+  )
 }
 
 function Cat() {
-  return <div>Meow!</div>
+  return (
+    <div>Meow!</div>
+  )
+}
+
+function User(props) {
+  return (
+    <div>Hi, I am {props.name}, and {props.age} years old</div>
+  )
+}
+
+// User のデフォルト値を定義
+User.defaultProps = {
+  age: 1
 }
 
 // クラスコンポーネント
