d1563722e4a1175d42c0ccb941e6675c09a2ea6d
takiguchi-yu <8340629+takiguchi-yu@users.noreply.github.com>
2019-12-01 01:28:40 +0900

    React components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+// 関数コンポーネント
 
 function App() {
   return (
     <React.Fragment>
       <h1 className="foo">Hello, world!!</h1>
       <h2 className="foo">Hello, world!!</h2>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
     </React.Fragment>
   );
 }
+
+function Cat() {
+  return <div>Meow!</div>
+}
+
+// クラスコンポーネント
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <h1 className="foo">Hello, world!!</h1>
+//         <h2 className="foo">Hello, world!!</h2>
+//       </React.Fragment>
+//     )
+//   }
+// }
 
 export default App;
