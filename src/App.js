979e61b09be47dd3c51f37f266b3779bc80becea
takiguchi-yu <8340629+takiguchi-yu@users.noreply.github.com>
2019-12-01 03:29:03 +0900

    learn state and setState

--- src/App.js
+++ src/App.js
@@ -1,82 +1,39 @@
-// import React, { Component } from 'react';
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
 /**
- * プロパティに対する型チェックを行う
+ * prop-types はイミュータブル(変更不可)
+ * state は ミュータブル（変更可能）
  */
 
-// 関数コンポーネント
-
 function App() {
-  const profiles = [
-    {
-      name: "Ichiro", 
-      age: 30,
-    },
-    {
-      name: "yamada",
-      age: 50,
-    },
-    {
-      name: "tanaka",
-      age: 1,
-    }
-  ]
-
-  return (
-    <React.Fragment>
-      <h1 className="foo">Hello, world!!</h1>
-      <h2 className="foo">Hello, world!!</h2>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
-      <User name={"Taro"} age={10} />
-      <User name={"Hanalo"} age={5} />
-      {
-        profiles.map((profile, index) => {
-          return <User name={profile.name} age={profile.age} key={index} />
-        })
-      }
-    </React.Fragment>
-  )
+  return <Counter></Counter>
 }
 
-function Cat() {
-  return (
-    <div>Meow!</div>
-  )
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    console.log(this.state)
+    this.state = { count: 0 }
+  }
+
+  handlePlusButton = () => {
+    this.setState({ count: this.state.count + 1 })
+  }  
+
+  handleMinusButton = () => {
+    this.setState({ count: this.state.count - 1 })
+  }  
+
+  render() {
+    console.log(this.state)
+    return (
+      <React.Fragment>
+        <div>count: { this.state.count } </div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    )
+  }
 }
-
-function User(props) {
-  return (
-    <div>Hi, I am {props.name}, and {props.age} years old</div>
-  )
-}
-
-// User のデフォルト値を定義
-// User.defaultProps = {
-//   age: 1
-// }
-
-// User の型を定義
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired,
-}
-
-// クラスコンポーネント
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <h1 className="foo">Hello, world!!</h1>
-//         <h2 className="foo">Hello, world!!</h2>
-//       </React.Fragment>
-//     )
-//   }
-// }
 
 export default App;
