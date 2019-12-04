557403a65b5fca823f40e9962bc6c0ef7dfd85e9
takiguchi-yu <y.takiguti@gmail.com>
2019-12-04 23:51:04 +0900

    implement delete feature

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,13 +1,27 @@
 import _ from 'lodash'
-import { READ_EVENTS } from '../actions'
+import { 
+  CREATE_EVENT,
+  READ_EVENTS,
+  READ_EVENT,
+  DELETE_EVENT,
+  UPDATE_EVENT,
+} from '../actions'
 
 // index.js に値を渡せるように export 
 // reducer は関数として定義する
 export default (events = {}, action) => {
   switch (action.type) {
     case READ_EVENTS:
-      console.log(_.mapKeys(action.response.data, 'id'))
       return _.mapKeys(action.response.data, 'id')
+    case CREATE_EVENT:
+    case READ_EVENT:
+    case UPDATE_EVENT:
+      const data = action.response.data
+      return { ...events, [data.id]: data }
+    case DELETE_EVENT:
+      delete events[action.id]
+      // 新しいメモリ空間上に更新後のオブジェクトを返却
+      return { ...events }   // 「...」スプレッド演算子（スプレッド構文）
     default:
       return events
   }
